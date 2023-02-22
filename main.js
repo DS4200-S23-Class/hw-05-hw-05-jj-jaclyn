aa806f33d7121a115e2917798f31e8f189965c8d
jake497726 <jung.jin@northeastern.edu>
2023-02-22 11:59:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -75,8 +75,8 @@ d3.csv("data/scatter-data.csv").then((data) => {
           .on("click", function() {
   // Check if the point already has a border
   var hasBorder = d3.select(this).attr("stroke") === "black";
-  let cx = this.getAttribute("cx");
-  let cy = this.getAttribute("cy");
+  let cx = this.getAttribute("cx") / 50;
+  let cy = (500 - this.getAttribute("cy")) / 50;
 
   // create new text
   let text1 = "Last point clicked: "
