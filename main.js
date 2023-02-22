fa7b0bfa1f92cf0eb3654b9fd86729f091e84c2e
jake497726 <jung.jin@northeastern.edu>
2023-02-22 09:39:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+let points = document.getElementsByTagName("circle");
+
+// loop to make second click unavaible 
+for (let i = 0; i < points.length; i++) {
+    
+    // check each point for clicks
+    let point = points[i];
+    point.addEventListener("click", function(){
+
+                  // click then add border
+                  this.classList.toggle("Border");
+
+                  //find the cx and cy
+                  let cx = this.getAttribute("cx") / 50;
+                  let cy = (500 - this.getAttribute("cy")) / 50;
+
+                  // create new text
+                  let text1 = "Last point clicked: "
+                  let text2 = "(" + cx +"," + cy + ")"
+  
+                  // show the text information
+                  document.getElementById("text1").innerHTML = text1;
+                  document.getElementById("text2").innerHTML = text2;
+}
+)}
+
 // creating frame
 // declare constant
 const FRAME_HEIGHT = 500
@@ -58,7 +84,6 @@ d3.csv("data/scatter-data.csv").then((data) => {
      .style("fill", "blue");
     };
   
-
 
   // Add event listeners
     FRAME1.selectAll(".point")
